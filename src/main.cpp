2ed8a18c3836d9ce2ba679b3ac5af6ee4406c1fe
Travis <mundyt4@mymacewan.ca>
2023-12-21 17:20:20 -0700

    Create generate matrix

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include <random>
 #include <chrono>
 
-float randomNum() {
+
+//create a random num by using the current time to generate the seed for it
+double randomNum() {
     // use the high resolution clock to generate a seed for the random num
     auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
     std::mt19937 generator(static_cast<unsigned>(seed));
@@ -11,29 +13,25 @@ float randomNum() {
     float min = 0.0f;
     float max = 10.0f;
 
-    std::uniform_real_distribution<float> distribution(min, max);
+    std::uniform_real_distribution<double> distribution(min, max);
+
+    //generate the float
+    return std::round(distribution(generator)* 100)/100;
 }
 
-// create generate matrices
+// create generate matrices 
 Matrix genMatrix(int32_t rows, int32_t cols) {
     Matrix m(rows, cols);
-
-
     for (int i = 0; i < rows; i++) {
-        for (int i = 0; j < cols; j++)
-            m.insert(, 0,);
+        for (int j = 0; j < cols; j++)
+            m.insert(randomNum(),i,j);
     }
     return m;
 }
 
 
 int main() { 
-    Matrix m(100000,100000);
-    //m.display();
-    std::cout << '\n';
-    m.insert(1.0, 0, 0);
-    m.insert(2.0, 1, 0);
-    m.insert(3.0, 0, 1);
-    m.insert(4.0, 1, 1);
-    //m.display();
+    Matrix m1 = genMatrix(5,5);
+    Matrix m1 = genMatrix(5,5);
+    m1.display();
 }
