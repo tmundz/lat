32919ba1c0da59934e0492327905bb895506cbdd
Travis <mundy.travis@gmail.com>
2024-02-08 15:31:29 -0700

    Determinants work

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "matrix.h"
 #include <chrono>
-#include <mpi.h>
 #include <random>
 
 // create a random num by using the current time to generate the seed for it
@@ -31,21 +30,16 @@ Matrix genMatrix(int32_t rows, int32_t cols) {
 }
 
 int main() {
-  MPI_Init(NULL, NULL);
-  int world_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-  // Get the rank of the process
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-  // Get the name of the processor
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int name_len;
-  MPI_Get_processor_name(processor_name, &name_len);
   Matrix m1 = genMatrix(2, 2);
   Matrix m2 = genMatrix(2, 2);
-  m1.display();
-  std::cout << std::endl;
+  std::vector<std::vector<double>> values2 = {{1.0, 2.0, 3.0}, {4.0, 3.0, 6.0}, {7.0, 8.0, 10.0}};
+  Matrix m4(values2);
+  
+  m4.display();
+  printf("\n");
+  double det = m4.determinant();
+  printf("%lf", det);
+  /*std::cout << std::endl;
   m2.display();
   std::cout << std::endl;
 
@@ -55,12 +49,5 @@ int main() {
 
   Matrix m4 = m1.sub(m2);
   m4.display();
-  std::cout << std::endl;
-
-  // Print off a hello world message
-  printf("Hello world from processor %s, rank %d out of %d processors\n",
-         processor_name, world_rank, world_size);
-
-  // Finalize the MPI environment.
-  MPI_Finalize();
+  std::cout << std::endl;*/
 }
--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -32,6 +32,35 @@ TEST(MatrixTestEqual, Equal) {
     EXPECT_TRUE(mat1 == mat2);
 }
 
+TEST(MatrixTestDet, NonSquare) {
+    std::vector<std::vector<double>> noDet = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}, {10.0, 11.0, 12.0}};
+    Matrix mNoDet(noDet);
+    // Assuming your determinant method throws an exception for non-square matrices.
+    EXPECT_THROW(mNoDet.determinant(), std::invalid_argument);
+}
+
+TEST(MatrixTestDet, Det2x2) {
+    std::vector<std::vector<double>> values1 = {{1.0, 2.0}, {3.0, 4.0}};
+    Matrix m1(values1);
+    EXPECT_DOUBLE_EQ(m1.determinant(), -2.0); // det = 1*4 - 2*3 = -2
+}
+
+TEST(MatrixTestDet, Det3x3) {
+    std::vector<std::vector<double>> values2 = {{1.0, 2.0, 3.0}, {4.0, 3.0, 6.0}, {7.0, 8.0, 10.0}};
+    Matrix m2(values2);
+    EXPECT_DOUBLE_EQ(m2.determinant(), 19.0);
+}
+
+TEST(MatrixTestDet, Det4x4) {
+    std::vector<std::vector<double>> values3 = {{5.0, 80.0, -2.0, 3.0}, {10.0, 5.0, 12.0, 76.0}, {2.0, 7.0, 23.0, 65.0}, {2.0, 13.0, 26.0, 32.0}};
+    Matrix m3(values3);
+    // Provide the expected determinant value for this test or use EXPECT_NO_THROW if you just want to ensure it doesn't throw an error.
+    EXPECT_NO_THROW(m3.determinant());
+    // If you know the expected determinant, replace the above line with:
+    // EXPECT_DOUBLE_EQ(m3.determinant(), expected_value);
+    EXPECT_DOUBLE_EQ(m3.determinant(),645414.0);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
